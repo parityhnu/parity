2dfa5176b8ba3f3f7c8285b509c0ba967ca024a0
xubinqing <592654201@qq.com>
2019-04-12 17:09:03 +0800

    完整的评论model 以及id特定的设置

--- app/src/main/java/com/binqing/utilproject/data/model/CommentReturnModel.java
+++ app/src/main/java/com/binqing/utilproject/data/model/CommentReturnModel.java
@@ -1,46 +1,33 @@
 package com.binqing.utilproject.data.model;
 
+import com.binqing.utilproject.data.annotation.Member;
+import com.binqing.utilproject.data.object.CommentReturnObject;
+import com.binqing.utilproject.data.object.JDCommentObject;
+import com.binqing.utilproject.data.object.TBCommentObject;
+import com.binqing.utilproject.data.object.TMCommentObject;
+
 import java.util.List;
 
 public class CommentReturnModel {
 
+    @Member(order = 0)
     private int maxPage;
 
+    @Member(order = 1)
     private List<JDCommentModel> jdCommentModels;
 
+    @Member(order = 2)
     private List<TBCommentModel> tbCommentModels;
 
+    @Member(order = 3)
     private List<TMCommentModel> tmCommentModels;
 
-    public int getMaxPage() {
-        return maxPage;
-    }
-
-    public void setMaxPage(int maxPage) {
-        this.maxPage = maxPage;
-    }
-
-    public List<JDCommentModel> getJdCommentModels() {
-        return jdCommentModels;
-    }
-
-    public void setJdCommentModels(List<JDCommentModel> jdCommentModels) {
-        this.jdCommentModels = jdCommentModels;
-    }
-
-    public List<TBCommentModel> getTbCommentModels() {
-        return tbCommentModels;
-    }
-
-    public void setTbCommentModels(List<TBCommentModel> tbCommentModels) {
-        this.tbCommentModels = tbCommentModels;
-    }
-
-    public List<TMCommentModel> getTmCommentModels() {
-        return tmCommentModels;
-    }
-
-    public void setTmCommentModels(List<TMCommentModel> tmCommentModels) {
-        this.tmCommentModels = tmCommentModels;
+    public CommentReturnObject toObject() {
+        CommentReturnObject object = new CommentReturnObject();
+        object.setMaxPage(maxPage);
+        object.setJDCommentObjects(JDCommentObject.fromModels(jdCommentModels));
+        object.setTBCommentObjects(TBCommentObject.fromModels(tbCommentModels));
+        object.setTMCommentObjects(TMCommentObject.fromModels(tmCommentModels));
+        return object;
     }
 }
--- app/src/main/java/com/binqing/utilproject/data/model/TBCommentModel.java
+++ app/src/main/java/com/binqing/utilproject/data/model/TBCommentModel.java
@@ -1,29 +1,51 @@
 package com.binqing.utilproject.data.model;
 
-import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.binqing.utilproject.data.annotation.Member;
+import com.binqing.utilproject.data.object.TBCommentObject;
 
 import java.util.List;
 
-@Document(collection = "comments_tb")
-public class TBCommentModel extends BaseCommentModel{
+public class TBCommentModel {
 
+    @Member(order = 0)
+    private String gid;
+
+    @Member(order = 1)
+    private int index;
+
+    @Member(order = 2)
+    private long ctime;
+
+    @Member(order = 3)
+    private String rateContent;
+
+    @Member(order = 4)
+    private List<String> pics;
+
+    @Member(order = 5)
+    private String displayUserNick;
+
+    @Member(order = 6)
+    private String content;
+
+    @Member(order = 7)
     private String auctionSku;
 
+    @Member(order = 8)
     private List<String> attendpics;
 
-    public String getAuctionSku() {
-        return auctionSku;
-    }
-
-    public void setAuctionSku(String auctionSku) {
-        this.auctionSku = auctionSku;
-    }
-
-    public List<String> getAttendpics() {
-        return attendpics;
-    }
-
-    public void setAttendpics(List<String> attendpics) {
-        this.attendpics = attendpics;
+    public TBCommentObject toObject() {
+        TBCommentObject object = new TBCommentObject();
+        object.setContent(content);
+        object.setAttendpics(attendpics);
+        object.setAuctionSku(auctionSku);
+        object.setCtime(ctime);
+        object.setRateContent(rateContent);
+        object.setDisplayUserNick(displayUserNick);
+        object.setGid(gid);
+        object.setIndex(index);
+        object.setPics(pics);
+        return object;
     }
 }
