d496998237dd700968ca3cefe35522148d68f94a
xubinqing <592654201@qq.com>
2019-04-16 14:37:20 +0800

    更改数据库名称为uid

--- app/src/main/java/com/binqing/utilproject/Consts/Consts.java
+++ app/src/main/java/com/binqing/utilproject/Consts/Consts.java
@@ -1,10 +1,12 @@
 package com.binqing.utilproject.Consts;
 
+import com.binqing.utilproject.ParityApplication;
+
 public class Consts {
 
     //数据库相关
     public static int DATABASE_VERSION = 3;
-    public static String DATABASE_NAME = "util_project_database.db";
+    public static String DATABASE_NAME = String.valueOf(ParityApplication.getInstance().getUserId());
     public static String DATABASE_PRIMARY_KEY_NAME = "p_id";
     public static String DATABASE_SQLITE_MASTER_TABLE_NAME = "s_master";
     public static String DATABASE_SQLITE_MASTER_Column_NAME = "s_name";
